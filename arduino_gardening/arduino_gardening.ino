216a1d8cd279642bac0a5c990d3c61b0feaaf292
Philipp Fuerholz <fuerholz@gmx.ch>
2020-01-26 12:27:39 +0100

    bugfix reading brightness values

--- arduino_gardening/arduino_gardening.ino
+++ arduino_gardening/arduino_gardening.ino
@@ -19,7 +19,6 @@ void setup()
   humidityValue = analogRead(A0);
   brightnessValue = analogRead(A1);
   lock_read = 0;
-  init_timer_1();
 }
 
 void loop()
@@ -32,14 +31,7 @@ void loop()
   {
     brightnessValue = analogRead(A1);
   }
-  power_adc_disable();
-  power_spi_disable();
-  power_timer0_disable();
-  power_timer2_disable();
-  sleep_cpu();
 
-  sleep_disable();
-  power_all_enable(); 
 }
 
 void requestEvent()
@@ -64,12 +56,12 @@ void requestEvent()
     if ((lock_read & 2) == 0)
     {
       lock_read = lock_read |  2;
-      byte h_high = humidityValue >> 8;
+      byte h_high = brightnessValue >> 8;
       Wire.write(h_high);
     }
     else
     {
-      byte h_low = humidityValue;
+      byte h_low = brightnessValue;
       Wire.write(h_low);
       lock_read = lock_read & B11111101;
     } 
@@ -88,18 +80,4 @@ void receiveEvent(int buffer)
   }
 }
 
-//Timer_1 initialisieren
-void init_timer_1 ()
-{
-  cli();        // Interrupts ausschalten
-  TCCR1A = 0;   // Register Reset
-  TCCR1B = 0;
-  TIMSK1 = 0;
-  TIMSK1 |= (1 << TOIE1); // Overflow Interrupt enable
-  //Timer-Counter-Startwert eine Vorladung zuweisen, damit dieser ca. 4 Sekunden hochzaehlt
-  //kein Wert zugewiesen heisst,dass es bis zum Overflow ca. 4,19s dauert ( bei 16 MHz und Prescaler 1024 (Overflow bei 65536))
-  TCNT1 = 0;
-  TCCR1B |= (1 << CS12) | (1 << CS10);  // Prescaler 1024
-  sei();        // Interrupts einschalten
-}
 
